e97f4ab3e6c86be32ac8198fb1763393026a1c3e
jflores45 <100002922+jflores45@users.noreply.github.com>
2023-05-24 20:11:16 -0400

    Update edit.py

--- insta485/views/accounts/edit.py
+++ insta485/views/accounts/edit.py
@@ -1,7 +1,10 @@
 """Edit account page for logged in user."""
 import flask
-from flask import request
+from flask import request, redirect, session, make_response
 import insta485
+from insta485.views.post import handle_file
+from insta485.views.image import download_file
+
 
 
 @insta485.app.route('/accounts/edit/', methods=['GET', 'POST'])
@@ -15,9 +18,84 @@ def show_edit():
 
 def do_the_edit():
     """Edit account."""
-    return "You did the edit in!"
+    if flask.session['username'] == None:
+        return flask.redirect("/accounts/login/")
 
+
+    
+
+    fullname = request.form.get('fullname')
+    email = request.form.get('email')
+    
+
+    connection = insta485.model.get_db()
+    cur = connection.execute(
+        "SELECT * "
+        "FROM users "
+        "WHERE username = ? ",
+        (session['username'], )
+    )
+    user = cur.fetchone()
+    
+    if flask.request.files['file'] == None or flask.request.files['file'] == "":
+        print("CCIONTEKNSDLKFN ",file.content_type)
+        print("in if", user['filename'])
+        file = download_file(user['filename'])
+    else:
+        print("CCIONTEKNSDLKFN ",file.content_type)
+        print("in else", flask.request.files['file'])
+        file = handle_file(flask.request.files['file'])
+
+    if fullname == None or fullname == "":
+        fullname = user['fullname']
+    if email == None or  email == "":
+        email = user['email']
+
+
+    print(file)
+    connection.execute(
+        "UPDATE users "
+        "SET fullname = ?, email = ?, filename = ? "
+        "WHERE username = ? ",
+        (fullname, email, file, session['username'])
+    )
+
+
+
+    connection.execute(
+        "UPDATE users "
+        "SET fullname = ?, email = ?, filename = ? "
+        "WHERE username = ? ",
+        (fullname, email, file, session['username'])
+    )
+    connection.commit()
+
+    cur = connection.execute(
+        "SELECT * "
+        "FROM users "
+        "WHERE username = ? ",
+        (session['username'], )
+    )
+    user = cur.fetchone()
+
+
+
+    
+    # return redirect("/users/" + session['username'] + "/")
+    return flask.redirect("/accounts/edit/")
 
 def show_the_edit_form():
     """Display /accounts/edit form."""
-    return flask.render_template("edit.html")
+    user = session['username']
+    connection = insta485.model.get_db()
+    cur = connection.execute(
+        "SELECT fullname, email AS email "
+        "FROM users "
+        "WHERE username = ? ",
+        (user, )
+    )
+    context = cur.fetchall()
+    context = context[0]
+
+
+    return flask.render_template("edit.html", **context)
