e1b3bde0bc2556db1dbb2092ba0700fab89cd3f0
jflores45 <100002922+jflores45@users.noreply.github.com>
2023-05-25 10:15:03 -0400

    Update posts.py pagination

--- insta485/api/posts.py
+++ insta485/api/posts.py
@@ -14,72 +14,99 @@ def get_resources():
     print(flask.jsonify(resources), "XXXXXXX")
     return flask.jsonify(resources)
 
-# @insta485.app.route('/api/v1/posts/<int:postid_url_slug>/')
-# def get_post(postid_url_slug):
-#     """Return post on postid.
-#     Example:
-#     {
-#       "created": "2017-09-28 04:33:28",
-#       "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
-#       "owner": "awdeorio",
-#       "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
-#       "ownerShowUrl": "/users/awdeorio/",
-#       "postShowUrl": "/posts/1/",
-#       "url": "/api/v1/posts/1/"
-#     }
-#     """
-#     context = {
-#         "created": "2017-09-28 04:33:28",
-#         "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
-#         "owner": "awdeorio",
-#         "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
-#         "ownerShowUrl": "/users/awdeorio/",
-#         "postid": "/posts/{}/".format(postid_url_slug),
-#         "url": flask.request.path,
-#     }
-#     return flask.jsonify(**context)
-
 # still need user authentication 
-def query_database_post(user_log):
-    "Query database for postid/url"
-  
-    connection = insta485.model.get_db()
-    #spec: "each post is made by a user which the logged in user follows or the post is made by the logged in user. ""
-   
-    cur = connection.execute(
-        "SELECT postid FROM posts WHERE owner = ?",
-          (user_log,)
-      )
-    posts_owner = cur.fetchall()
+# def query_database_post(user_log):
+#     "Query database for postid/url"
+#     connection = insta485.model.get_db()
     
-    cur2 = connection.execute(
-        "SELECT postid FROM posts WHERE owner IN (SELECT username FROM following WHERE follower = ?)",
-        (user_log,)
-      )
-    posts_following = cur2.fetchall()
-    posts = posts_owner + posts_following
-    return posts
+#     #spec: "each post is made by a user which the logged in user follows or the post is made by the logged in user. ""
+#     cur = connection.execute(
+#         "SELECT postid"
+#         "FROM posts"
+#         "ORDER BY posts.created DESC "
+#         "WHERE owner = ?",
+#         (user_log,)
+#       )
+#     posts_id = cur.fetchall()
+      
+#     cur2 = connection.execute(
+#         "SELECT postid"
+#         "FROM posts"
+#         "ORDER BY posts.created DESC "
+#         "WHERE owner IN (SELECT username FROM following WHERE follower = ?)",
+#         (user_log,)
+#       )
+#     postsid_following = cur2.fetchall()
+#     postsid = posts_id + postsid_following
+#     return postsid
+
+def query_database_post(user_log, postid_lte=None, size=10, page=1):
+    """Query database for postid/url."""
+    connection = insta485.model.get_db()
+
+    # Retrieve posts made by the logged in user or users they follow in one query
+    query = (
+        "SELECT postid "
+        "FROM posts "
+        "WHERE owner = ? "
+        "OR owner IN (SELECT username FROM following WHERE follower = ?) "
+        "ORDER BY created DESC"
+    )
+    params = (user_log, user_log)
+    cur = connection.execute(query, params)
+    posts_id = cur.fetchall()
+
+    # Apply postid_lte 
+    if postid_lte is not None:
+        posts_id = [post for post in posts_id if post[0] <= postid_lte]
+
+    # Apply pagination
+    offset = (page - 1) * size
+    limited_posts_id = posts_id[offset:offset + size]
+
+    return [post[0] for post in limited_posts_id]
+
 
 @insta485.app.route('/api/v1/posts/')
 def get_post():
     """Return 10 newest post urls/ids"""
-   
     if "username" not in flask.session:
-        flask.abort(403)
+      flask.abort(403)
 
+    #Pagination parameters
+    postid_lte = request.args.get('postid_lte', default=None, type=int)
+    size = request.args.get('size', default=10, type=int)
+    page = request.args.get('page', default=1, type=int)
+
+    # error check size and page parameters
+    if size < 0 or page < 0:
+        response = {
+            "message": "Bad Request",
+            "status_code": 400
+        }
+        return flask.jsonify(response), 400
+    
     user_log = flask.session['username']
-    # Retrieve the 10 posts from func set limit
-    posts = query_database_post(user_log)
-    limited_posts = posts[:10]
+
+    # Retrieve the 10 posts from func 
+    posts = query_database_post(user_log, postid_lte, size, page)
+    
+    offset = (page - 1) * size
+    limited_posts = posts[offset:offset + size]
 
     results = [
-        {"postid": posts[0], 
+        {"postid": post[0], 
          "url": f"/api/v1/posts/{post[0]}/"}
        for post in limited_posts
      ]
+    
+    next_page = ""
+    if len(posts) > size * page:
+        next_page = f"/api/v1/posts/?postid_lte={postid_lte}&size={size}&page={page + 1}"
+
     response = {
-        "next": "",
+        "next": next_page,
         "results": results,
-        "url": flask.request.path,
+        "url": flask.request.path + f"?postid_lte={postid_lte}&size={size}&page={page}",
     }
-    return flask.jsonify(response)
+    return flask.jsonify(response), 200
