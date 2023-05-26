726268c4eb158c6ac827d5fdd3c724fc056e4e87
jflores45 <100002922+jflores45@users.noreply.github.com>
2023-05-26 17:40:54 -0400

    Update __init__.py

--- insta485/views/__init__.py
+++ insta485/views/__init__.py
@@ -3,7 +3,7 @@ import insta485.api
 from insta485.views.index import show_index
 from insta485.views.explore import show_explore
 from insta485.views.accounts.account import show_account
-from insta485.views.accounts.login import show_login, do_logout
+from insta485.views.accounts.login import show_login, do_logout, require_authentication
 from insta485.views.accounts.check_password import check_password, hash_password
 from insta485.views.accounts.create import show_create
 from insta485.views.accounts.edit import show_edit
@@ -12,3 +12,4 @@ from insta485.views.accounts.delete import show_delete
 from insta485.views.post import handle_file
 from insta485.views.image import download_file
 from insta485.api.posts import get_resources, get_post, get_posts
+from insta485.api.likes import create_like
