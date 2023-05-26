97087f7595821f703caae362dbeb31dbd565f62e
TallanGroberg <tallan.taven@gmail.com>
2023-05-26 19:52:54 -0400

    fix lowercase error aws

--- insta485/js/Comments.jsx
+++ insta485/js/Comments.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import Comment from "./comment";
+import Comment from "./Comment";
 
 const Comments = ({ comments }) => {
   return (
