c3c70afa4db29ac20df58568b554cf7db03a5fcd
jflores45 <100002922+jflores45@users.noreply.github.com>
2023-05-26 21:12:28 -0400

    Update likes.py 3/5 need comments test cases

--- insta485/api/likes.py
+++ insta485/api/likes.py
@@ -4,22 +4,23 @@ from flask import request, session, jsonify
 import insta485
 
 
+# like helpers
 def like_exists(postid, user_log):
     connection = insta485.model.get_db()
     cur = connection.execute(
-        "SELECT EXISTS (SELECT 1 FROM likes WHERE owner = ? AND postid = ?)",
+        "SELECT postid "
+        "FROM likes "
+        "WHERE owner = ? AND postid = ?",
         (user_log, postid)
     )
-    result = cur.fetchone()
+    result = cur.fetchall()
     if result is not None and len(result) == 1:
-        print("no: 1")
         return True
     else:
         return False
 
 def get_like_id(postid, owner):
     connection = insta485.model.get_db()
-    print("no: 3")
     cur = connection.execute(
         "SELECT likeid FROM likes WHERE postid = ? AND owner = ?",
         (postid, owner)
@@ -29,12 +30,12 @@ def get_like_id(postid, owner):
         return result['likeid']
     return None
 
+
 @insta485.app.route('/api/v1/likes/', methods=['POST'])
 def create_like():
     """Create a new 'like' for a specific post."""
     postid = request.args.get('postid', type=int)
-    print("POSTID:", postid)
-
+    # print("POSTID:", postid)
     if flask.request.authorization:
         user_log = flask.request.authorization['username']
     else:
@@ -52,7 +53,6 @@ def create_like():
         }
         return jsonify(likes), 200
     else:
-        print("HERE: CORRECT")
         # Create a new like and return with a 201 response
         path = f"/api/v1/likes/{postid}/"
         owner = user_log
@@ -60,12 +60,12 @@ def create_like():
         connection = insta485.model.get_db()
         #get number of likes on this post
         cur = connection.execute(
-            "SELECT COUNT(*) FROM likes WHERE postid = ?",
+            "SELECT COUNT(*) AS count FROM likes WHERE postid = ?",
             (postid,)
         )
         result = cur.fetchone()
-        num_likes = result[0]
-        #add login user's like to likes table
+        num_likes = result['count']
+        # add login user's like to likes table
         connection.execute(
             # fix: add likesid/created?
             "INSERT INTO likes (owner, postid) "
@@ -79,3 +79,65 @@ def create_like():
             "url": path,
         }
         return jsonify(likes), 201
+
+
+# delete like helpers
+def exists(likeid):
+    connection = insta485.model.get_db()
+    cur = connection.execute(
+        "SELECT likeid "
+        "FROM likes "
+        "WHERE likeid = ?",
+        (likeid,)
+    )
+    result = cur.fetchall()
+    if result:
+        return True
+    else:
+        return False
+    
+
+def user_owns_like(likeid, user_log):
+    connection = insta485.model.get_db()
+    cur = connection.execute(
+        "SELECT likeid "
+        "FROM likes "
+        "WHERE likeid = ? AND owner = ?",
+        (likeid, user_log)
+    )
+    result = cur.fetchall()
+    if result is not None and len(result) == 1:
+        return True
+    else:
+        return False
+
+def delete_like_action(likeid):
+    connection = insta485.model.get_db()
+    print("done")
+    cur = connection.execute(
+        "DELETE FROM likes WHERE likeid = ?",
+        (likeid,)
+    )
+    connection.commit()
+
+@insta485.app.route('/api/v1/likes/<int:likeid>/', methods=['DELETE'])
+def delete_like(likeid):
+    """Delete a 'like'."""
+    if flask.request.authorization:
+        user_log = flask.request.authorization['username']
+    else:
+        user_log = session['username']
+    # Check if the like exists
+    if exists(likeid):
+        # Check if the user owns the like
+        if user_owns_like(likeid, user_log):
+            # Delete the like and return a 204 response
+            delete_like_action(likeid)
+            return jsonify({"message": "Forbidden", "status_code": 204}), 204
+        else:
+            # User does not own the like, return a 403 response
+            return jsonify({"message": "Forbidden", "status_code": 403}), 403
+    else:
+        print("no")
+        # Like does not exist, return a 404 response
+        return jsonify({"message": "Not Found", "status_code": 404}), 404
